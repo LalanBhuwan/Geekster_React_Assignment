db1029fa7c656a0e5b6d3b338a56e6f0981507ad
LalanBhuwan <bhuwanlalan475@gmail.com>
2022-11-05 10:38:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading1 containet_centre">About Us</h1>
+      <p className="p1 containet_centre">At Zappos.com, our purpose is simple:to live and deliver WOW.</p>
+      <p className="p2 containet_centre">Twenty years ago, We began as a small online retailer that only sold shoes. Today, we still sell shoes - as well as clothing handbags, accessories, and more. That "more" is providing the very best customer service, customer exprience, and company culture. We aim to inspire the world by showing it's possible to simultaneously deliver happiness to customers, employees, vendors, shareholders, and the community in a long-term, sustainable way.</p>
+      <br></br>
+      <p className="p3 containet_centre">We hope that in the future people won't even realize we started selling shoes online. Instead, they'll know Zappos as a </p>
+      <span className="spanP">service company that just hoppens to sell_____.</span>
+    </div>
+  );
+}
+
+export default App;
